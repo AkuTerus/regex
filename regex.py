f6b57bcf411c2dabca3822cfee7a302346b2a35a
AkuTerus <66345580+AkuTerus@users.noreply.github.com>
2023-06-08 17:52:05 +0700

    Update regex.py

--- regex.py
+++ regex.py
@@ -51,3 +51,32 @@ def validasi_kartu_kredit(nomor_kartu):
         return 'Valid Platinum'
     else:
         return 'Valid Reguler'
+
+def validasi_kartu_kredit(nomor_kartu):
+    if len(nomor_kartu) !=16 :
+        return "Tidak valid"
+    elif not re.match(r"^[4-6]\d{16}$", nomor_kartu):
+        return "tidak valid"
+    elif re.search(r"8{4}", nomor_kartu):
+        return 'Valid Platinum'
+    else:
+        return 'Valid Reguler'
+# nomor_kartu = '4388884888814526'
+#cara ming
+def validasi_kartu_kredit(nomor_kartu):
+    try :
+        nomor_kartu == int(nomor_kartu)
+    except:
+        return "Tidak valid" 
+
+    kartu = str(nomor_kartu)
+    if len(kartu) !=16:
+        return 'Tidak valid'
+    else:
+        if kartu[0] in "456":
+            if "8888" in kartu:
+                return 'Valid Platinum'
+            else:
+                return 'Valid Reguler'
+        else:
+            return 'Tidak valid'
